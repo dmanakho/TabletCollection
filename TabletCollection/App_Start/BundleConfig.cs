e2d651d3ca215a7c4ef6fd6c22c51d607237c58b
Dmitry Manakhov <dmanakho@Hotmail.com>
2018-03-22 14:02:38 -0400

    Initial Staging. Empty Project with bootstrap and first model started.

--- TabletCollection/App_Start/BundleConfig.cs
+++ TabletCollection/App_Start/BundleConfig.cs
@@ -23,9 +23,12 @@ namespace TabletCollection
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new StyleBundle("~/Content/cssstyles").Include(
+                      "~/Content/Flatly/bootstrap-flatly.css",
                       "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
         }
     }
 }
---
+++ TabletCollection/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TabletCollection.Models
+{
+    public class Student
+    {
+        [Required]
+        [DisplayName("First Name"), MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last Name"), MaxLength(50)]
+        public string LastName { get; set; }
+
+        [DisplayName("Full Name")]
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+        [Required]
+        [DisplayName("Class Of")]
+        public int ClassOf { get; set; }
+    }
+}
