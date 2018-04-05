5e65307a80eb8b93389d82a8ce0772fe48606bd6
Dmitry Manakhov <dmitry_manakhov@caryacademy.org>
2018-04-05 18:55:42 -0400

    Added UTC to Local Time and reverse resolution in Automapper

--- TabletCollection/App_Start/AutoMapperConfig.cs
+++ TabletCollection/App_Start/AutoMapperConfig.cs
@@ -5,6 +5,7 @@ using System.Web;
 using AutoMapper;
 using TabletCollection.Models;
 using TabletCollection.ViewModels;
+using TabletCollection.Infrastructure;
 
 namespace TabletCollection
 {
@@ -15,13 +16,29 @@ namespace TabletCollection
             CreateMap<Student, StudentViewModel>();
             CreateMap<StudentViewModel, Student>();
 
-            CreateMap<TabletViewModel, Tablet>().ReverseMap();
+            CreateMap<Tablet, TabletViewModel>()
+                .ForMember(dest => dest.CreatedOn, opt => opt.MapFrom(src => src.CreatedOn.HasValue ? src.CreatedOn.Value.ToLocalTime() : src.CreatedOn))
+                .ForMember(dest => dest.UpdatedOn, opt => opt.MapFrom(src => src.UpdatedOn.HasValue ? src.UpdatedOn.Value.ToLocalTime() : src.UpdatedOn));
+            CreateMap<TabletViewModel, Tablet>()
+                .ForMember(dest => dest.CreatedOn, opt => opt.MapFrom(src => src.CreatedOn.HasValue ? src.CreatedOn.Value.ToUniversalTime() : src.CreatedOn))
+                .ForMember(dest => dest.UpdatedOn, opt => opt.MapFrom(src => src.UpdatedOn.HasValue ? src.UpdatedOn.Value.ToUniversalTime() : src.UpdatedOn));
+
 
             CreateMap<Collection, CollectionViewModel>()
-                .ConstructUsing(c=> new CollectionViewModel());
-            CreateMap<CollectionViewModel, Collection>();
+                .ForMember(dest => dest.CreatedOn, opt => opt.MapFrom(src => src.CreatedOn.HasValue ? src.CreatedOn.Value.ToLocalTime() : src.CreatedOn))
+                .ForMember(dest => dest.UpdatedOn, opt => opt.MapFrom(src => src.UpdatedOn.HasValue ? src.UpdatedOn.Value.ToLocalTime() : src.UpdatedOn))
+                .ConstructUsing(c => new CollectionViewModel());
+            CreateMap<CollectionViewModel, Collection>()
+                .ForMember(dest => dest.CreatedOn, opt => opt.MapFrom(src => src.CreatedOn.HasValue ? src.CreatedOn.Value.ToUniversalTime() : src.CreatedOn))
+                .ForMember(dest => dest.UpdatedOn, opt => opt.MapFrom(src => src.UpdatedOn.HasValue ? src.UpdatedOn.Value.ToUniversalTime() : src.UpdatedOn));
 
-            CreateMap<Collection, BFFViewModel>().ReverseMap();
+            CreateMap<Collection, BFFViewModel>()
+                .ForMember(dest => dest.CreatedOn, opt => opt.MapFrom(src => src.CreatedOn.HasValue ? src.CreatedOn.Value.ToLocalTime() : src.CreatedOn))
+                .ForMember(dest => dest.UpdatedOn, opt => opt.MapFrom(src => src.UpdatedOn.HasValue ? src.UpdatedOn.Value.ToLocalTime() : src.UpdatedOn));
+            CreateMap<BFFViewModel, Collection>()
+                .ForMember(dest => dest.CreatedOn, opt => opt.MapFrom(src => src.CreatedOn.HasValue ? src.CreatedOn.Value.ToUniversalTime() : src.CreatedOn))
+                .ForMember(dest => dest.UpdatedOn, opt => opt.MapFrom(src => src.UpdatedOn.HasValue ? src.UpdatedOn.Value.ToUniversalTime() : src.UpdatedOn));
+
         }
     }
 }
---
+++ TabletCollection/Infrastructure/AutoMapperResolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using TabletCollection.Models;
+using TabletCollection.ViewModels;
+
+namespace TabletCollection.Infrastructure
+{
+    public class UTCToLocalTimeResolver : IValueResolver<Tablet, TabletViewModel, DateTime?>
+    {
+        public DateTime? Resolve(Tablet tablet, TabletViewModel tabletViewModel, DateTime? destMember, ResolutionContext context)
+        {
+            return tablet.CreatedOn.HasValue ? tablet.CreatedOn.Value.ToLocalTime() : tablet.CreatedOn;
+        }
+    }
+}
